07bb189fa75d846f454a28d2ce7881063cfc6066
Julius Wilmar <wilmarjulius@gmail.com>
2023-03-22 10:20:08 +0800

    Calculator Only

---
+++ app/src/main/java/com/example/midterm/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        Button button3 = (Button) findViewById(R.id.button3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Calculator.class);
+                startActivity(intent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ToDoList.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
